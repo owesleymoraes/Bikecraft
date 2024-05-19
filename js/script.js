686cf35416b01e96f906b433c554bf5848eaea75
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-05-19 13:06:27 -0300

    feat: adding js in menu principal

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("teste");
+const links = document.querySelectorAll(".header-menu a");
+
+function activeLink(link) {
+  const url = window.location.href;
+  const hrefMenuClicked = link.href;
+
+  if (url.includes(hrefMenuClicked)) {
+    link.classList.add("active");
+  }
+}
+
+links.forEach(activeLink);
